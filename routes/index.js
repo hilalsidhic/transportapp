f3457fb04ba99ceb977a26253e5ab33fd76dd53c
hilalsidhic <hilalsidhic21@gmail.com>
2021-09-17 19:57:37 +0530

    airline options added

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,12 @@ router.get('/optionPage', function(req, res, next) {
   res.render('user/option')
 })
 
+router.get('/airplane', function(req, res, next) {
+  res.render('user/airplane')
+})
+
+router.get('/countrylistapi',(req, res, next) => {
+  res.json('https://api.printful.com/countries')
+})
+
 module.exports = router;
